60f988b1d87b94a0eeceb003747626de42849458
Masterflo3004 <100784148+Masterflo3004@users.noreply.github.com>
2025-02-20 18:23:25 +0100

    implemented the compiler logic (and first time using github copilot)

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -1,8 +1,9 @@
-use std::io::Read;
+use std::io::{Read, Write};
 
 use std::fs;
 use std::path::Path;
 use crate::compiler;
+use toml;
 
 pub fn build(file:String){
 
@@ -13,13 +14,67 @@ pub fn build(file:String){
     file.read_to_string(&mut code).expect("An Error Occured while reading the file");
 
     //compiles the code into one Vector (Vec<String>) and into one String)
-    let (libs, rs_code) = compiler::compile(&code)
+    let (libs, rs_code) = compiler::compile(&code);
 
-    //initializes a new cargo project
+    //initializes a new cargo project using system calls
+    let _cargo = std::process::Command::new("cargo")
+        .arg("new")
+        //removing the file ending
+        .arg(path.file_stem().unwrap().to_str().unwrap())
+        //changing the directory to the parent directory
+        .current_dir(path.parent().unwrap())
+        .output()
+        .expect("Could not create a new cargo project");
 
-    //writes the the String into src/main
 
-    //Reads the used Librarys from the Vector and copies the files into /src
+    // removes all files from the src directory
+    fs::remove_dir_all(path.parent().unwrap().join(path.file_stem().unwrap().to_str().unwrap()).join("src")).expect("Could not remove the src directory");
+    //creates a new src directory
+    fs::create_dir(path.parent().unwrap().join(path.file_stem().unwrap().to_str().unwrap()).join("src")).expect("Could not create the src directory");
+
+    //creates a new src/main.rs file and writes the compiled code into it
+    let mut file = fs::File::create(path.parent().unwrap().join(path.file_stem().unwrap().to_str().unwrap()).join("src").join("main.rs")).expect("Could not create the new main.rs file");
+    file.write_all(rs_code.as_bytes()).expect("Could not write the compiled code into the new main.rs file");
+    //Reads the used Librarys from the Vector and copys their code into src/<lib>.rs
+    
+    //reads the librarys code from lib/<lib>.toml (the code variable) and writes it into src/<lib>.rs
+    for lib in libs{
+        //converts the toml file from toml to a HashMap
+        let lib_path = path.parent().unwrap().join(path.file_stem().unwrap().to_str().unwrap()).join("lib").join(lib.clone()+".toml");
+        //checks if the toml file exists
+        if !lib_path.exists(){
+            println!("The toml file for the Node {} does not exist", lib);
+            break;
+        }
+
+        let mut file = fs::File::open(lib_path).expect("Could not open the toml file");
+        let mut toml = String::new();
+        file.read_to_string(&mut toml).expect("Could not read the toml file");
+        let lib_code = toml::from_str::<std::collections::HashMap<String, String>>(&toml).expect("Could not convert the toml file into a HashMap");
+
+
+        //creates the new file
+        let mut file = fs::File::create(path.parent().unwrap().join(path.file_stem().unwrap()
+        .to_str().unwrap())
+        .join("src").join(lib.clone()+".rs"))
+        .expect("Could not create the new file");
+
+        //writes the code into the file
+        file.write_all(lib_code.get("code").unwrap().as_bytes()).expect("Could not write the code into the file");
+
+        //reads the cargo dependencies from the used librarys and downloads them
+        let dependencies = lib_code.get("dependencies").unwrap();
+        let dependencies = dependencies.split(" ");
+        for dep in dependencies{
+            let _cargo = std::process::Command::new("cargo")
+                .arg("add")
+                .arg(dep)
+                .output()
+                .expect("Could not install the dependencies");
+        }
+
+        
+    }
 
     //reads the cargo dependencies from the used librarys and downloads them
 }
--- src/compiler.rs
+++ src/compiler.rs
@@ -1,7 +1,4 @@
 use std::collections::HashMap;
-pub fn create_code (line: &String){
-
-}
 
 
 
@@ -17,6 +14,8 @@ pub fn compile (code: &String) -> (Vec<String>, String){
     //5.1convert the line from a &&str into a Vector
     //5.2check if the line has enough parts (input/node/output)
     //5.3adds the node to the list of required librarys
+
+
     //5.4splits input variables into vector
     //5.5checks if input variables are declared 
     //5.6splits output variables into a Vector
@@ -30,27 +29,87 @@ pub fn compile (code: &String) -> (Vec<String>, String){
     let lines = code.split("\n");
     let line_l = lines.collect::<Vec<&str>>();
     //created the vector for the libs
-    let mut lib_l:Vec<String>;
+    let mut lib_l:Vec<String> = Vec::new();
+    //creates the hashmap for the variables
     let mut var_l: HashMap<String, bool> = HashMap::new();
+
+    //creates the vector for the code
+    let mut code_l: Vec<String> = Vec::new();
+
+    code_l.push("fn main() {".to_string());
+
+    //creates the counter for the lines
     let mut num = 1;
 
     //4
     for x in &line_l{
-        let statement = x.split("/").collect::<Vec<&str>>();
+        if x.to_string().starts_with("#"){
+            continue;
+        }
+
+
+        let statement: Vec<&str> = x.splitn(3, '!').collect();
 
 
         if statement.iter().count() != 3{
-            println!("There was an error at line {} please check if you declared every in and output or if oyu wrote _ if none is required", num);
+            print!("{}", statement.iter().count());
+            println!("There was an error at line {} please check if you declared every in and output or if you wrote _ if none is required", num);
         }
         let lib = statement[1].to_string(); //gets the used Node of the command and puts its name into the lib
-        lib_l.push(lib);
+        lib_l.push(lib.clone()); //adds the lib to the lib list
 
+        let input = statement[0].split(":").collect::<Vec<&str>>();
+        for x in &input{
+            if *x != "_"{
+                if var_l.contains_key(*x) != true{
+                    println!("there was an error splitting argument {}", num);
+                }
+            }
+        }
+
+        let output = statement[2].split(":").collect::<Vec<&str>>();
+        for x in &output{
+            var_l.insert(x.to_string(), true);
+        }
+
+        //creates the code line
+        let mut code_line = "".to_string();
+        code_line = code_line + "let ";
+        for x in &output{
+            //NOTE: add the check if the types are matching
+            code_line = code_line + x + ",";
+        }
+        code_line = code_line + " = ";
+
+        //adds the function lib from dependency lib 
+        code_line = code_line + &lib + "::" + &lib + "(";
+        for x in &input{
+            code_line = code_line + x + ",";
+        }
+        //remove the last ,
+        code_line = code_line.trim_end_matches(',').to_string();
+
+        code_line = code_line + ");";
+
+        //adds the code line to the code list
+        code_l.push(code_line);
 
 
 
 
         num += 1;
     }
+    code_l.push("}".to_string());
 
-    return (lib_l, "".to_string());
+    //inserts all the required librarys
+    for x in &lib_l{
+        code_l.insert(0, "mod ".to_string() + x + ";");
+    }
+
+    //imports the librarys
+    for x in &lib_l{
+        code_l.insert(lib_l.iter().count(), "use ".to_string() + x);
+    }
+
+    return (lib_l, code_l.join("\n"));
 }
